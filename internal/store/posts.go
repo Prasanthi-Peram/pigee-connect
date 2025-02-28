438f8d1f4f6a39d3c7eabccf24d55e60b0b1fde9
Prasanthi-Peram <prasanthiperam29@gmail.com>
2025-02-28 19:55:44 +0530

    Define Post and User structures

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -7,11 +7,22 @@ import (
 	"github.com/lib/pq"
 )
 
+type Post struct{
+	ID int64 `json:"id"`
+	Content string `json:"content"`
+	Title string `json:"title"`
+	UserID int64 `json:"user_id"`
+	Tags []string `json:"tags"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+
+}
+
 type PostStore struct{
 	db *sql.DB
 }
 
-func (s *PostStore) Create(ctx context.Context) error{
+func (s *PostStore) Create(ctx context.Context, post *Post) error{
 	
 	return nil
 }
--- internal/store/users.go
+++ internal/store/users.go
@@ -10,6 +10,14 @@ type UsersStore struct{
 	db *sql.DB
 }
 
-func (s *UsersStore) Create(ctx context.Context) error{
+type User struct{
+	ID int64 `json:"id"`
+	Username string `json:"username"`
+	Email string `json:"email"`
+	Password string `json:"-"`
+	CreatedAt string `json:"created_at"`
+}
+
+func (s *UsersStore) Create(ctx context.Context, user *User) error{
 	return nil
 }
