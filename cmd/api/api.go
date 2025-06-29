f40ef0525a44260facec561dfd4d7144d27543fe
Prasanthi-Peram <prasanthiperam29@gmail.com>
2025-06-29 12:14:03 +0530

    feat: Implement server shutdown

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -6,6 +6,13 @@ import(
 	"time"
 	"fmt"
 
+	"context"
+	"errors"
+
+	"os"
+	"os/signal"
+	"syscall"
+
 	"go.uber.org/zap"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
@@ -106,7 +113,7 @@ func (app *application) mount() *chi.Mux{
 
 	// Group of Routes
 	r.Route("/v1", func(r chi.Router) {
-		r.With(app.BasicAuthMiddleware()).Get("/health", app.healthCheckHandler)
+		r.Get("/health", app.healthCheckHandler)
 
 		docsURL := fmt.Sprintf("%s/swagger/doc.json",app.config.addr)
 		r.Get("/swagger/*",httpSwagger.Handler(httpSwagger.URL(docsURL)))
@@ -166,7 +173,32 @@ func (app *application) run(mux http.Handler) error{
 		ReadTimeout: time.Second*10,
 		IdleTimeout: time.Minute,
 	}
+
+	shutdown := make(chan error)
+	go func() {
+		quit := make(chan os.Signal, 1)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		s := <-quit
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+		defer cancel()
+
+		app.logger.Infow("signal caught", "signal", s.String())
+		shutdown <- srv.Shutdown(ctx)
+	}()
+
 	app.logger.Infow("server has started","addr", app.config.addr,"env",app.config.env)
 
-	return srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+	err = <-shutdown
+	if err != nil {
+		return err
+	}
+	app.logger.Infow("server has stopped", "addr", app.config.addr, "env", app.config.env)
+	return nil
 }
